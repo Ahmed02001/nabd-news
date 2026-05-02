2b7bcf59076368cad3d951f443ca447615ef8fd1
Ahmed02001 <ahmedhamdyy787@gmail.com>
2026-05-02 12:10:28 +0300

    addd company page

---
+++ app/companies/page.js
@@ -0,0 +1,146 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import {
+  Container,
+  Box,
+  Typography,
+  Divider,
+  Card,
+  CardMedia,
+  CardContent,
+  Stack,
+} from "@mui/material";
+import Hero from "@/components/Hero";
+import Categories from "@/components/Categories";
+import NewsCard from "@/components/NewsCard";
+import Trending from "@/components/Trending";
+import Grid from "@mui/material/Grid";
+import LatestNewsCard from "@/components/LatestNewsCard";
+import getNews from "@/lib/categoryAPI";
+
+export default function Page() {
+  const categories = [
+    "أمازون",
+    "ميتا",
+    "تسلا",
+    "سامسونج",
+    "تويتر",
+    "مايكروسوفت",
+    "ابل",
+    "جوجل",
+    "بي إم دبليو",
+  ];
+
+  const [articles, setArticles] = useState([]);
+  const [query, setQuery] = useState("شركات");
+  useEffect(() => {
+    getNews(query).then((res) => setArticles(res));
+  }, [query]);
+
+  return (
+    <Container
+      disableGutters
+      sx={{
+        display: "grid",
+        gridTemplateColumns: "repeat(3, 1fr)",
+        gap: 2,
+        mt: 20,
+      }}
+    >
+      <Container className="col-span-3  lg:col-span-2 mx-2 md:mx-10 lg:mx-30">
+        {articles[0] && <Hero article={articles[0]} />}
+
+        <Box
+          sx={{
+            width: "100%",
+            py: 2,
+            position: "sticky",
+            top: "125px",
+            left: " 50%",
+            zIndex: 50,
+            bgcolor: "#eee",
+          }}
+        >
+          <Stack
+            direction="row"
+            spacing={1}
+            sx={{
+              width: "100%",
+              overflowX: "scroll",
+              scrollbarWidth: "none",
+              overflowStyle: "none",
+            }}
+          >
+            <Categories categories={categories} onSelect={setQuery} />
+          </Stack>
+        </Box>
+        <Typography
+          variant="h5"
+          sx={{
+            width: "150px",
+            mt: 4,
+            mb: 4,
+            pb: 1,
+            textDecoration: "none",
+            borderBottom: "1px solid #E63946",
+            color: "text.primary",
+            transition: "1",
+            "&:hover": {
+              color: "primary.main",
+            },
+            fontWeight: "700",
+          }}
+        >
+          أحدث الأخبار
+        </Typography>
+
+        <Grid container spacing={3}>
+          {articles.slice(1, 7).map((article, i) => (
+            <Grid xs={12} sm={6} md={4} key={i}>
+              <NewsCard article={article} />
+            </Grid>
+          ))}
+        </Grid>
+
+        <Trending />
+      </Container>
+      <Container
+        disableGutters
+        className="col-span-3 md:col-span-1 min-w-90.5"
+        sx={{
+          p: 2,
+          boxShadow: "0px 0px 3px #E63946",
+          borderRadius: "10px",
+          height: "1000px",
+        }}
+      >
+        <Typography
+          variant="h5"
+          sx={{
+            width: "150px",
+            mb: 3,
+            pb: 1,
+            textDecoration: "none",
+            borderBottom: "1px solid #E63946",
+            color: "text.primary",
+            transition: "1",
+            "&:hover": {
+              color: "primary.main",
+            },
+            fontWeight: "700",
+          }}
+        >
+          أحدث الأخبار
+        </Typography>
+        <Box sx={{ display: "flex", flexDirection: "column", gap: "20px" }}>
+          {articles.slice(1, 7).map((article, i) => (
+            <Grid key={i}>
+              <LatestNewsCard article={article} />
+            </Grid>
+          ))}
+        </Box>
+      </Container>
+    </Container>
+  );
+}
