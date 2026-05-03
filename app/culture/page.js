f5347cbcb89991eb13bcf7709643c4a976e49a14
Ahmed02001 <ahmedhamdyy787@gmail.com>
2026-05-03 15:30:59 +0300

    add culture news page

---
+++ app/culture/page.js
@@ -0,0 +1,30 @@
+import { Box, Typography } from "@mui/material";
+import getNews from "@/lib/categoryAPI";
+import NewsList from "@/components/NewsList";
+
+export default async function page() {
+  const query = "ثقافة";
+  const articles = await getNews(query);
+
+  return (
+    <Box sx={{ marginTop: "150px" }} className="mx-2 md:mx-10 lg:mx-30">
+      <Typography
+        variant="h5"
+        sx={{
+          mb: 2,
+          textDecoration: "none",
+          borderBottom: "1px solid primary.main",
+          color: "text.primary",
+          "&:hover": {
+            color: "primary.main",
+          },
+          fontWeight: "700",
+        }}
+      >
+        🔥 الأكثر قراءة
+      </Typography>
+
+      <NewsList initialArticles={articles} query={query} />
+    </Box>
+  );
+}
